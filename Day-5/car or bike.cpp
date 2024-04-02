ee38fd1157b526408d5f76fa0d87e07b8faf8234
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-04-02 16:11:25 +0530

    Q5

---
+++ Day-5/car or bike.cpp
@@ -0,0 +1,24 @@
+/*
+An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). 
+A company manager wants to make the production of both types of vehicle according to the given data below:
+
+1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
+2nd data, Total number of wheels = W
+
+Input :
+200  -> Value of V
+540   -> Value of W
+
+Output :
+TW =130 FW=70
+
+Explanation:
+130+70 = 200 vehicles
+(70*4)+(130*2)= 540 wheels
+
+
+Constraints :
+    2 <= W
+    W % 2 = 0
+    V < W
+*/
